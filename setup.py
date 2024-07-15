334e9026f74e0da875f9fa32e8ce03e2aa1b33e5
Hugo Haas <hugoh@hugoh.net>
2024-07-14 23:19:06 -0500

    Gave up on setuptools pylint error…

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 """Setup code"""
 
-from setuptools import setup
+from setuptools import setup  # pylint: disable=import-error
 
 setup(
     name="tmo_reboot",
     version="0.1",
     description="A script to reboot the T-Mobile Home Internet 5G Gateways",
     long_description="""
-    A lightweight, cross-platform Python 3 script that can reboot the T-Mobile Home Internet
-    Arcadyan and Nokia 5G Gateways.
+    A lightweight, cross-platform Python 3 script that can reboot the
+    T-Mobile Home Internet Arcadyan and Nokia 5G Gateways.
     """,
     url="https://github.com/hugoh/tmo-reboot",
     author="hugoh",

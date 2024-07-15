4f720768eec3e2fac0aad5b055fe13ad7b519d93
Hugo Haas <hugoh@hugoh.net>
2024-07-14 21:04:40 -0500

    Fixed flake8 issue

--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@ setup(
     name="tmo_reboot",
     version="0.1",
     description="A script to reboot the T-Mobile Home Internet 5G Gateways",
-    long_description="A lightweight, cross-platform Python 3 script that can reboot the T-Mobile Home Internet Arcadyan and Nokia 5G Gateways.",
+    long_description="""
+    A lightweight, cross-platform Python 3 script that can reboot the T-Mobile Home Internet
+    Arcadyan and Nokia 5G Gateways.
+    """,
     url="https://github.com/hugoh/tmo-reboot",
     author="hugoh",
     license="MIT",
